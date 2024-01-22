b908f4fa24d4e967721d2902e49274b805c634ea
Lior <liormdevops@gmail.com>
2024-01-22 13:05:03 +0200

    working on fluentd-config-own file not completed

--- scripts/FU-fluentd.sh
+++ scripts/FU-fluentd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl delete cm -n fluentd fluentd-config                                                    
-helm uninstall fluentd -n fluentd                                                       
-kubectl apply -f app-of-apps/fluentd-cm-custom.yaml                                                  
+helm uninstall fluentd -n fluentd
+
+kubectl apply -f app-of-apps/fluentd-config-own.yaml                                                  
 helm install -n fluentd fluentd bitnami/fluentd -f app-of-apps/files/fluentd-values.yaml
