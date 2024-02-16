504f8f1d038edb8a883b0b3890ee6776999cba4c
Lior <liormdevops@gmail.com>
2024-02-16 11:36:04 +0200

    fluentd refresh script bye bye

--- fluentd-refresh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl delete cm -n elastic fluentd-config                                                    
-helm uninstall fluentd -n elastic
-
-kubectl apply -f app-of-apps/files/fluentd-cm.yaml                                                  
-helm install -n elastic fluentd bitnami/fluentd -f app-of-apps/files/fluentd-values.yaml
